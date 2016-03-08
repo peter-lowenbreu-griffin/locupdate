0bd0943372dcf187511a6740f96a6ef9bff0997c
unknown <JPITADENIYA@CL-JPITADENIYA.Virtusa.com>
2016-03-08 15:42:07 +0800

    starting fresh

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-var express = require('express');
-var app = express();
-
-
-app.get('/test', function (req, res) {
-       res.end( "Hello" );
-   });
-})
-
-var server = app.listen(8080, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
+console.log("Hello World");
