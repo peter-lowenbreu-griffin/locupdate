a54c6bfa5a28de091fb554018a5c43f1435f9d54
unknown <JPITADENIYA@CL-JPITADENIYA.Virtusa.com>
2016-03-08 16:11:23 +0800

    simple server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+var express = require('express');
+var app = express();
+var LISTEN_PORT = 8080;
+
+
+app.get('/', function (req, res) {
+	var date = new Date();
+   res.end( date.toString() );
+})
+
+
+var server = app.listen(LISTEN_PORT, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
